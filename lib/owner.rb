58ddc358986c66d82bf22c89e12dd7343402535d
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-10 04:08:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,7 +50,7 @@ class Owner
   end
   
   def list_pets
-    puts "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dogs, and #{@pets[:cats].size} cats."
+    "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dogs, and #{@pets[:cats].size} cats."
   end
   
   def self.all
